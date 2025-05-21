de80f642dcf02b6ef3fe155039d705c4869b003e
brooks-claire91 <brooks.claire91@gmail.com>
2025-05-21 09:17:28 -0500

    init pokmon add

---
+++ PokemonReviewApp/Migrations/20250520133940_AddPokemonNumnber.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PokemonReviewApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPokemonNumnber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PokemonNumber",
+                table: "Pokemon",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PokemonNumber",
+                table: "Pokemon");
+        }
+    }
+}
---
+++ PokemonReviewApp/Models/Pokemon.cs
@@ -0,0 +1,14 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public int PokemonNumber { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Owners { get; set; }
+        public ICollection<PokemonOwner> PokemonOwners { get; set; }
+        public ICollection<PokemonCategory> PokemonCategories { get; set; }
+    }
+}
