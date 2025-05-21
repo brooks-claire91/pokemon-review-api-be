0d9d945854e72a00e7ffd7bcb6ef4ec787b96b7b
brooks-claire91 <brooks.claire91@gmail.com>
2025-05-21 12:04:06 -0500

    created first endput

---
+++ PokemonReviewApp/Interfaces/IPokemonRepository.cs
@@ -0,0 +1,12 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interfaces
+{
+    public interface IPokemonRepository
+    {
+        Task<ICollection<Pokemon>> GetAllPokemonsAsync();
+        Pokemon GetPokemon(int pokemonNumber);
+        Pokemon GetPokemon(string name);
+        bool PokemonExist(int pokemonNumber);
+    }
+}
---
+++ PokemonReviewApp/Repository/PokemonRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+using System.Linq;
+
+namespace PokemonReviewApp.Repository
+{
+    public class PokemonRepository : IPokemonRepository
+    {
+        private readonly DataContext _context;
+        public PokemonRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ICollection<Pokemon>> GetAllPokemonsAsync()
+        {
+            return await _context.Pokemon
+                .OrderBy(p => p.PokemonNumber)
+                .ToListAsync();
+        }
+
+        public async Task<ICollection<Pokemon>> GetPokemonsByNameAsync()
+        {
+            return await _context.Pokemon
+                .OrderBy(p => p.PokemonNumber)
+                .ToListAsync();
+        }
+    }
+}
